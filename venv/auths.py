10eaf412c01a5ee5fe07fa74681a546692a79587
MfaizanTariq <mfaizantariq@hotmail.com>
2022-12-04 14:16:35 -0500

    ver 2.1

--- venv/auths.py
+++ venv/auths.py
@@ -48,28 +48,19 @@ def signup_google():
 def callback():
     from venv.controller import User_Pre_Req, Add_Def_User
 
+    time.sleep(1)
+    if not session["state"] == request.args["state"]:
+        return redirect(url_for("views.index"))
+
     flow.fetch_token(authorization_response=request.url)
-
-    if not session["state"] == request.args["state"]:
-        return redirect(url_for("views.index"))
-
     print("stuck at 0")
-    time.sleep(3)
-
-    if not session["state"] == request.args["state"]:
-        return redirect(url_for("views.index"))
+    time.sleep(1)
 
     credentials = flow.credentials
-    print("stuck at 1")
     request_session = requests.session()
-    print("stuck at 2")
-    print("stuck at 3")
-    cached_session = cachecontrol.CacheControl(request_session)
-    print("stuck at 4")
-    print("stuck at 5")
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    #cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=request_session)
 
-    print("stuck at 6")
     id_info = id_token.verify_oauth2_token(
         id_token=credentials._id_token,
         request=token_request,
